3432d3a5d7f959e82264569187fde0d8bf001632
Azetus <izana723@hotmail.com>
2025-05-08 19:34:58 +0800

    Patch TickManager.TicksGame prefix

--- Source/Patches/TickManagerTicksGamePatch.cs
+++ Source/Patches/TickManagerTicksGamePatch.cs
@@ -1,34 +1,34 @@
-﻿//using HarmonyLib;
-//using PauseWalker.Context;
-//using Verse;
+﻿using HarmonyLib;
+using PauseWalker.Utilities;
+using Verse;
 
-//namespace PauseWalker.Patches
-//{
-//    [HarmonyPatch(typeof(TickManager), nameof(TickManager.TicksGame), MethodType.Getter)]
-//    public static class TickManagerTicksGamePatch
-//    {
-//        public static bool Prefix(ref int __result)
-//        {
+namespace PauseWalker.Patches
+{
+    [HarmonyPatch(typeof(TickManager), nameof(TickManager.TicksGame), MethodType.Getter)]
+    public static class TickManagerTicksGamePatch
+    {
+        public static bool Prefix(ref int __result)
+        {
 
-//            if (Current.Game == null || Find.TickManager == null)
-//            {
-//                return true; 
-//            }
+            if (Current.Game == null || Find.TickManager == null)
+            {
+                return true;
+            }
 
-//            try
-//            {
-//                if (Find.TickManager.CurTimeSpeed == TimeSpeed.Paused)
-//                {
-//                    __result = SimulatedTickManager.SimulatedTicksGameInt;
-//                    return false;
-//                }
-//            }
-//            catch (Exception e)
-//            {
-//                Log.Warning($"[PauseWalker] Exception in TickManagerTicksGamePatch: {e}");
-//                return true; // 保底：原方法执行
-//            }
-//            return true;
-//        }
-//    }
-//}
+            try
+            {
+                if (Find.TickManager.CurTimeSpeed == TimeSpeed.Paused)
+                {
+                    __result = SimulatedTickManager.SimulatedTicksGameInt;
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                Log.Warning($"[PauseWalker] Exception in TickManagerTicksGamePatch: {e}");
+                return true; // 保底：原方法执行
+            }
+            return true;
+        }
+    }
+}
