55acb63d0d681319344e5d690004a023c1e7551b
Azetus <izana723@hotmail.com>
2025-06-28 15:11:43 +0800

    cast ability while pawn is down

---
+++ Source/CompProperties/PauseWalker_Regenerate.cs
@@ -0,0 +1,77 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace PauseWalker.CompProperties
+{
+    public class PauseWalker_Regenerate : HediffComp
+    {
+        private List<Hediff_Injury> tmpInjuries = new List<Hediff_Injury>();
+        private List<Hediff_MissingPart> tmpMissingParts = new List<Hediff_MissingPart>();
+
+        public PauseWalkerHediffCompProperties_Regenerate Props => (PauseWalkerHediffCompProperties_Regenerate)this.props;
+
+        public override void CompPostTick(ref float severityAdjustment)
+        {
+            base.CompPostTick(ref severityAdjustment);
+
+            float regenBudget = Props.healAmount;
+
+            if (regenBudget <= 0f || Pawn?.health == null) return;
+
+            // 疤痕
+            var candidates = Pawn.health.hediffSet.hediffs
+                .Where(hd => hd.IsPermanent() || hd.def.chronic)
+                .ToList();
+
+            if (candidates.TryRandomElement(out Hediff toCure))
+            {
+                HealthUtility.Cure(toCure);
+            }
+
+            // 伤口
+            Pawn.health.hediffSet.GetHediffs<Hediff_Injury>(ref tmpInjuries, (Hediff_Injury h) => !h.IsPermanent() && h.Severity > 0f);
+            foreach (var injury in tmpInjuries)
+            {
+                float healAmount = Mathf.Min(regenBudget, injury.Severity);
+                injury.Heal(healAmount);
+                regenBudget -= healAmount;
+                Pawn.health.hediffSet.Notify_Regenerated(healAmount);
+                if (regenBudget <= 0f) return;
+            }
+
+            // 身体部位
+            Pawn.health.hediffSet.GetHediffs<Hediff_MissingPart>(ref tmpMissingParts, h =>
+                h.Part.parent != null &&
+                !tmpInjuries.Any(x => x.Part == h.Part.parent) &&
+                Pawn.health.hediffSet.GetFirstHediffMatchingPart<Hediff_MissingPart>(h.Part.parent) == null &&
+                Pawn.health.hediffSet.GetFirstHediffMatchingPart<Hediff_AddedPart>(h.Part.parent) == null
+            );
+
+            if (tmpMissingParts.Any())
+            {
+                Hediff_MissingPart missingPart = tmpMissingParts.First();
+                BodyPartRecord part = missingPart.Part;
+
+                Pawn.health.RemoveHediff(missingPart);
+
+                // 移除部位缺失，添加伤口
+                var added = Pawn.health.AddHediff(HediffDefOf.Misc, part);
+                float partHealth = Pawn.health.hediffSet.GetPartHealth(part);
+                added.Severity = Mathf.Max(partHealth - 1f, partHealth * 0.9f);
+
+                Pawn.health.hediffSet.Notify_Regenerated(partHealth - added.Severity);
+            }
+        }
+    }
+
+    public class PauseWalkerHediffCompProperties_Regenerate : HediffCompProperties
+    {
+        public float healAmount = 100.0f;
+
+        public PauseWalkerHediffCompProperties_Regenerate()
+        {
+            this.compClass = typeof(PauseWalker_Regenerate);
+        }
+    }
+}
---
+++ Source/CustomAbility/PauseWalkerAbility.cs
@@ -0,0 +1,87 @@
+﻿using RimWorld;
+using RimWorld.Planet;
+using Verse;
+using Verse.AI.Group;
+
+namespace PauseWalker.CustomAbility
+{
+    public class PauseWalkerAbility : Ability
+    {
+        public PauseWalkerAbility() : base() { }
+        public PauseWalkerAbility(Pawn pawn) : base(pawn) { }
+        public PauseWalkerAbility(Pawn pawn, Precept sourcePrecept):base(pawn, sourcePrecept) { }
+        public PauseWalkerAbility(Pawn pawn, AbilityDef def):base(pawn, def) { }
+        public PauseWalkerAbility(Pawn pawn, Precept sourcePrecept, AbilityDef def) : base(pawn, sourcePrecept, def) { }
+        override public bool GizmoDisabled(out string reason)
+        {
+            if (this.CanCooldown && this.OnCooldown && (!this.def.cooldownPerCharge || this.RemainingCharges == 0))
+            {
+                reason = "AbilityOnCooldown".Translate(this.CooldownTicksRemaining.ToStringTicksToPeriod(true, false, true, true, false)).Resolve();
+                return true;
+            }
+            if (this.UsesCharges && this.RemainingCharges <= 0)
+            {
+                reason = "AbilityNoCharges".Translate();
+                return true;
+            }
+            if (!this.comps.NullOrEmpty<AbilityComp>())
+            {
+                for (int i = 0; i < this.comps.Count; i++)
+                {
+                    if (this.comps[i].GizmoDisabled(out reason))
+                    {
+                        return true;
+                    }
+                }
+            }
+            AcceptanceReport canCast = this.CanCast;
+            if (!canCast.Accepted)
+            {
+                reason = canCast.Reason;
+                return true;
+            }
+            Lord lord = this.pawn.GetLord();
+            if (lord != null)
+            {
+                AcceptanceReport report = lord.AbilityAllowed(this);
+                if (!report)
+                {
+                    reason = report.Reason;
+                    return true;
+                }
+            }
+            if (!this.pawn.Drafted && this.def.disableGizmoWhileUndrafted && this.pawn.GetCaravan() == null && !DebugSettings.ShowDevGizmos)
+            {
+                reason = "AbilityDisabledUndrafted".Translate();
+                return true;
+            }
+            if (this.pawn.DevelopmentalStage.Baby())
+            {
+                reason = "IsIncapped".Translate(this.pawn.LabelShort, this.pawn);
+                return true;
+            }
+            //if (this.pawn.Downed)
+            //{
+            //    reason = "CommandDisabledUnconscious".TranslateWithBackup("CommandCallRoyalAidUnconscious").Formatted(this.pawn);
+            //    return true;
+            //}
+            if (this.pawn.Deathresting)
+            {
+                reason = "CommandDisabledDeathresting".Translate(this.pawn);
+                return true;
+            }
+            if (this.def.casterMustBeCapableOfViolence && this.pawn.WorkTagIsDisabled(WorkTags.Violent))
+            {
+                reason = "IsIncapableOfViolence".Translate(this.pawn.LabelShort, this.pawn);
+                return true;
+            }
+            if (!this.CanQueueCast)
+            {
+                reason = "AbilityAlreadyQueued".Translate();
+                return true;
+            }
+            reason = null;
+            return false;
+        }
+    }
+}
